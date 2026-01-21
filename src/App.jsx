999ae170dbc86f3ad4c79ebd5ffbc511c137f19e
S.k. Parthasarathi Kar <parthasarathikar2002@gmail.com>
2026-01-21 20:06:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react'
 
 import './App.css'
-import { NavBar } from './components/nav/nav'
+import { NavBar } from './components/nav/Nav'
 import { Hero } from './components/hero/HeroSection'
 import { ImpactSec } from './components/main/impact/Impact'
 import { Stack } from './components/main/techStack/Stack'
